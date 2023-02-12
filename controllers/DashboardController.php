075bc008d0a91a1f448d2f0569d11b7f4ded7168
Juan Carlos <jcanosa1988@gmail.com>
2023-02-12 01:23:20 +0100

    Ventana Modal Tareas

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,28 +2,74 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
 use MVC\Router;
+
 
 class DashboardController{
     public static function index(Router $router){
-
         session_start();
         isAuth();
 
+        $id = $_SESSION['id'];
+
+        $proyectos = Proyecto::belongsTo('propietarioid', $id);
+
         $router->render('dashboard/index',[
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
 
     public static function crear_proyecto(Router $router){
         session_start();
         isAuth();
-
         $alertas = [];
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $proyecto = new Proyecto($_POST);
+
+            //Validacion
+            $alertas = $proyecto->validarProyecto();
+
+            if(empty($alertas)){
+                //Generar una url única
+                $hash = md5(uniqid());
+                $proyecto->url = $hash;
+
+                //Almacenar el creador del proyecto
+                $proyecto->propietarioid = $_SESSION['id'];
+
+                //Guardar el proyecto
+                $proyecto->guardar();
+
+                //Redireccionar
+                header('Location: /proyecto?id='.$proyecto->url);
+            }
+        }
 
         $router->render('dashboard/crear-proyecto',[
             'alertas' => $alertas,
             'titulo' => 'Crear Proyecto'
+        ]);
+    }
+
+    public static function proyecto(Router $router){
+        session_start();
+        isAuth();
+
+        $token = $_GET['id'];
+        if(!$token) header('Location: /dashboard');
+
+        //Revisar que ve el proyecto el propietario
+        $proyecto = Proyecto::where('url', $token);
+        
+        if($proyecto->propietarioid !== $_SESSION['id']){
+            header('Location: /dashboard');
+        }
+
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
         ]);
     }
 
